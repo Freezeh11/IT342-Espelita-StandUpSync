7e9ecabc848602d9ad8b48eb0dc556ab330d5ce3
LAB13_USER ON WS15 <L13Y12W15@LabHCI.net>
2026-04-11 10:54:19 +0800

    IT342 Phase 3 – Web Main Feature Completed

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,22 +3,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import ProjectPage from './components/ProjectPage';
 import ProfilePage from './components/ProfilePage';
 
 function App() {
-    return (
-        <div className="app-container">
-            <Router>
-                <Routes>
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/profile" element={<ProfilePage />} />
-                    <Route path="/" element={<Login />} />
-                </Routes>
-            </Router>
-        </div>
-    );
+  return (
+    <div className="app-container">
+      <Router>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/project" element={<ProjectPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
